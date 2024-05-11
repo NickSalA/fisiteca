ab5cb6cfee2acb105d4329e6a8b5fd53ae951de1
Nick <nicksalcedo717@gmail.com>
2024-05-10 22:05:27 -0500

    uwu

--- data/sistPedidoLibro.cpp
+++ data/sistPedidoLibro.cpp
@@ -3,9 +3,11 @@
 #include <fstream>
 #include <time.h>
 #include <string>
+#include <sstream>
 #include <windows.h>
 
 using namespace std;
+string xcodigo,xano;
 struct Libro {
     int codigo;
     string nombre;
@@ -20,33 +22,36 @@ void leerLibro(Libro libros[], int& n){
     if(Leer.fail()){
 		cout<<"Error en el archivo..."<<endl;
 		exit(1);
-
-	for (int i = 0; i < n; i++) {
-        Leer >> libros[i].codigo;
-        Leer >> libros[i].nombre;
-        Leer >> libros[i].genero;
-		Leer >> libros[i].autor;
-        Leer >> libros[i].anoPublicacion;
-        Leer >> libros[i].sinopsis;	
-	}
+    }
+    int i=0;
+    while(!Leer.eof()){
+        //lectura de datos
+        getline(Leer,xcodigo);
+        getline(Leer,libros[i].nombre);
+        getline(Leer,libros[i].genero);
+        getline(Leer,libros[i].autor);
+        getline(Leer,xano);
+        getline(Leer,libros[i].sinopsis);
+        //almacenar datos
+        istringstream(xcodigo) >> libros[i].codigo;
+        istringstream(xano)>>libros[i].anoPublicacion;
+        n++;
+        i++;
+    }
 	Leer.close();
-	}
+	
 }
 
 void agregarLibro(Libro libros[], int& n) {
-    
+    ofstream Grabacion("D:/Repos/fisiteca/data/output/libros.txt", ios::app);
     //Prob cuando lo compilen les salga error D:, si encuentran la forma de conectarlo a la nube en lugar del espacio de trabajo local colocanlo uu
     cout << "Sistema de pedido de libros" << endl;
     cout << "Ingresar los datos del libro" << endl;
 	//Lo de abajo es la base de datos, los cout se pueden cambiar de acuerdo al diseño final
     Libro newLibro;
-    /*for (int i = 0; n < i; i++){
-        newLibro.codigo = 
-    }
     cout << "Codigo: ";
     cin >> newLibro.codigo;
     fflush(stdin);
-    */
     cout << "Nombre: ";
     getline(cin, newLibro.nombre);
     cout << "Genero: ";
@@ -60,7 +65,6 @@ void agregarLibro(Libro libros[], int& n) {
     getline(cin, newLibro.sinopsis);
     libros[n] = newLibro;
     n++;
-    ofstream Grabacion("D:/Repos/fisiteca/data/output/libros.txt", ios::app);
     if (Grabacion.fail()) {
         cout << "Error en el archivo..." << endl;
         Sleep(2000);
@@ -79,7 +83,7 @@ void agregarLibro(Libro libros[], int& n) {
 int main() {
     Libro libros[100];
     int n = 0;
-    leerLibro(libros, n);
+    leerLibro(libros,n);
     int opcion;
     do {
         cout << "\n1. Agregar libro\n2. Leer libros\n3. Salir" << endl;
@@ -87,17 +91,21 @@ int main() {
         switch (opcion) {
             case 1:
                 agregarLibro(libros, n);
+                leerLibro(libros,n);
                 break;
             case 2:
+                int numlibro;
+                
                 for (int i = 0; i < n; i++) {
-                    cout << "\nLibro " << i + 1 << endl;
-                    cout << "Codigo: " << libros[i].codigo << endl;
-                    cout << "Nombre: " << libros[i].nombre << endl;
-                    cout << "Genero: " << libros[i].genero << endl;
-                    cout << "Autor: " << libros[i].autor << endl;
-                    cout << "Año de publicacion: " << libros[i].anoPublicacion << endl;
-                    cout << "Sinopsis: " << libros[i].sinopsis << endl;
+                    cout << "\nLibro " << i + 1<<": " <<libros[i].nombre<< endl;
                 }
+                cout<<"Seleccione su libro "<<endl;cin>>numlibro;
+                numlibro-=1;
+                cout<<"\nLibro: "<<libros[numlibro].nombre;
+                cout<<"\nGenero: "<<libros[numlibro].genero;
+                cout<<"\nAutor: "<<libros[numlibro].autor;
+                cout<<"\nAño de publicacion: "<<libros[numlibro].anoPublicacion;
+                cout<<"\nSipnosis: "<<libros[numlibro].sinopsis;
                 break;
             case 3:
                 cout << "Adios!" << endl;
@@ -105,7 +113,8 @@ int main() {
             default:
                 cout << "Opcion invalida" << endl;
         }
+        system("pause");
+        system("cls");
+
     } while (opcion!= 3);
-    return 0;
 }
-//ayuda Edu D:
