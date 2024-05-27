1f736e58580fc5b9d40a66e5260bd674bd3e623d
Nick <nicksalcedo717@gmail.com>
2024-05-27 00:17:25 -0500

    ddsfads

--- main.cpp
+++ main.cpp
@@ -4,37 +4,44 @@
 #include <stdlib.h>
 #include <conio.h>
 #include "data/userManagement.hpp"
-
+#include "menu/funciones.hpp"
 using namespace std;
 
 int main() {
-    int opc;
-    configurarConsolaUtf8();
-        system("cls");
-        cout << "MENU PRINCIPAL" << endl;
-        cout << "1. Ingresar como Usuario" << endl;
-        cout << "2. Ingresar como Administrador" << endl;
-        cout << "3. Registrar Usuario" << endl;
-        cout << "4. Salir" << endl;
-        cout << "Ingrese una opcion: ";
-        cin >> opc;
-        
-    switch (opc) {
-        case 1: 
-            menuUsuario();
-            break;
-        case 2: 
-            menuAdmin();
-            break;
-        case 3: 
-            registrarUsuario();
-            break;
-        case 4: 
-            cout<< "Saliendo...";
-            break;
-        default:    
-            cout << "Opcion invalida" << endl;
-            break; 
-}
-}
+    int opc = 0;
+    vector<string> opciones = {"Ingresar como Usuario", "Ingresar como Administrador", "Registrar Usuario", "Salir"};
+    coordXY pos = {7, 7};
 
+    configurarConsolaUtf8();
+    ocultarCursor();
+
+    while (true) {
+        system("cls");
+        dibujarTitulo(3, 1, "MENU PRINCIPAL");
+        dibujarMenu(pos.x, pos.y, opciones);
+        moverCursor({pos.x - 2, pos.y + opc});
+        cout << "==>";
+
+        char key = _getch();
+        if (key == 72 && opc > 0) { // Flecha arriba
+            opc--;
+        } else if (key == 80 && opc < opciones.size() - 1) { // Flecha abajo
+            opc++;
+        } else if (key == '\r') { // Enter
+            switch (opc) {
+                case 0: 
+                    menuUsuario();
+                    break;
+                case 1: 
+                    menuAdmin();
+                    break;
+                case 2: 
+                    registrarUsuario();
+                    break;
+                case 3: 
+                    cout << "Saliendo...";
+                    return 0;
+            }
+        }
+    }
+}
---
+++ menu/funciones.hpp
@@ -0,0 +1,126 @@
+#ifndef FUNCIONES_HPP
+#define FUNCIONES_HPP
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <conio.h>
+#include <limits>
+#include <locale>
+using namespace std;
+
+struct coordXY {
+        int x;
+        int y;
+        bool operator == (const coordXY& vec) { return (x == vec.x && y == vec.y); }
+        bool operator != (const coordXY& vec) { return !(*this == vec); }
+        coordXY operator + (const coordXY& vec) { return { x + vec.x, y + vec.y }; }
+        coordXY operator - (const coordXY& vec) { return { x - vec.x, y - vec.y }; }
+    };
+void gotoxy (int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void configurarConsolaUtf8() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+}
+
+void limpiarPantalla() {
+    system("cls");
+}
+
+void pausa() {
+    system("pause");
+}
+
+void dibujarCuadro(int x, int y, int ancho, int alto) {
+    for (int i = 0; i < ancho; i++) {
+        gotoxy(x + i, y);
+        cout << char(205);
+        gotoxy(x + i, y + alto);
+        cout << char(205);
+    }
+    for (int i = 0; i < alto; i++) {
+        gotoxy(x, y + i);
+        cout << char(186);
+        gotoxy(x + ancho, y + i);
+        cout << char(186);
+    }
+    gotoxy(x, y);
+    cout << char(201);
+    gotoxy(x + ancho, y);
+    cout << char(187);
+    gotoxy(x, y + alto);
+    cout << char(200);
+    gotoxy(x + ancho, y + alto);
+    cout << char(188);
+}
+
+void dibujarTitulo(int x, int y, string titulo) {
+    int padding = 2; // Espacio adicional a cada lado del título
+    gotoxy(x, y);
+    cout << char(201);
+    for (int i = 0; i < titulo.length() + 2 * padding; i++) {
+        cout << char(205);
+    }
+    cout << char(187);
+    gotoxy(x, y + 1);
+    cout << char(186);
+    for (int i = 0; i < padding; i++) {
+        cout << " ";
+    }
+    cout << titulo;
+    for (int i = 0; i < padding; i++) {
+        cout << " ";
+    }
+    cout << char(186);
+    gotoxy(x, y + 2);
+    cout << char(200);
+    for (int i = 0; i < titulo.length() + 2 * padding; i++) {
+        cout << char(205);
+    }
+    cout << char(188);
+}
+
+void dibujarMenu(int x, int y, vector<string> opciones) {
+    for (int i = 0; i < opciones.size(); i++) {
+        gotoxy(x, y + i);
+        cout << i + 1 << ". " << opciones[i];
+    }
+}
+
+void cursorVisible(bool visible) {
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO lpCursor;
+    lpCursor.bVisible = visible;
+    lpCursor.dwSize = 20;
+    SetConsoleCursorInfo(console, &lpCursor);
+}
+
+void ocultarCursor() {
+    cursorVisible(false);
+}
+
+void moverCursor(coordXY pos) {
+    gotoxy(pos.x, pos.y);
+}
+
+void moverCursorUP(int n) {
+    COORD coord;
+    coord.X = 0;
+    coord.Y = -n;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void moverCursorDOWN(int n) {
+    COORD coord;
+    coord.X = 0;
+    coord.Y = n;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}  
+
+#endif // USERMANAGEMENT_HPP
